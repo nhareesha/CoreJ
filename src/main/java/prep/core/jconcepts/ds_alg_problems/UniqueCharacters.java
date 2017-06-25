c5b100fa78612f36b7f50e4c21ff8ba1449dd79b
hareesha <hareesha1906@gmail.com>
2017-06-24 18:48:50 -0700

    Unique character String

---
+++ src/main/java/prep/core/jconcepts/ds_alg_problems/UniqueCharacters.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package prep.core.jconcepts.ds_alg_problems;
+
+/**
+ * @author Hareesha
+ * Jun 23, 2017 2017
+ */
+public class UniqueCharacters {
+
+	/**
+	 * IF string has unique characters or not?
+	 */
+	public static void main(String[] args) {
+		
+		String s = "abcdefggik";
+		char[] arr = s.toCharArray();
+		//ASCII characters
+		int[] a = new int[128];
+		
+		for(char b : arr){
+			a[b]++;
+			if(a[b]>1){
+				System.out.println("Not unique");
+				return;
+			}
+			
+		}
+		System.out.println("Unique");
+	}
+
+}
