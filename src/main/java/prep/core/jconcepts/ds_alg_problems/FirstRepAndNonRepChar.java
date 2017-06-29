1970186ce78fa28dcbe3d01f0865a732377bbd80
hareesha <hareesha1906@gmail.com>
2017-06-28 18:36:48 -0700

    repeated and non repeated charactes

---
+++ src/main/java/prep/core/jconcepts/ds_alg_problems/FirstRepAndNonRepChar.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package prep.core.jconcepts.ds_alg_problems;
+
+/**
+ * @author Hareesha
+ * Jun 28, 2017 2017
+ */
+public class FirstRepAndNonRepChar {
+
+	/**
+	 * Find First repeated and non repeated character
+	 */
+	public static void main(String[] args) {
+		String s = "helloh";
+		firstRepeatedChar(s);
+		firstNonRepeatedChar(s);
+	}
+	
+	public static void firstRepeatedChar(String s){
+		for(int i = 0;i<s.length();i++){
+			if(s.lastIndexOf(s.charAt(i))!=i){
+				System.out.println("First repeated charcter : "+s.charAt(i));
+				break;
+			}
+		}
+	}
+	
+	public static void firstNonRepeatedChar(String s){
+		for(int i = 0;i<s.length();i++){
+			if(s.lastIndexOf(s.charAt(i))==i){
+				System.out.println("First Non Repeated charcter : "+s.charAt(i));
+				break;
+			}
+		}
+	}
+}
