91eae38a4ed5b5fc9158dd3bc2e60592522c8790
hareesha <hareesha1906@gmail.com>
2017-06-28 17:27:13 -0700

    update

--- src/main/java/prep/core/jconcepts/ds_alg_problems/OneStringRotationOfAnother.java
+++ src/main/java/prep/core/jconcepts/ds_alg_problems/OneStringRotationOfAnother.java
@@ -19,20 +19,25 @@ public class OneStringRotationOfAnother {
        true
 	 */
 	public static void main(String[] args) {
-		String s1 = "JavaJ2eeStrutsHibernate";
-		 
-	    String s2 = "StrutsHibernateJavaJ2ee";
+		//String s1 = "JavaJ2ssStrutsHibernate";
+		// String s2 = "StrutsHibernateJavaJ2ss";
+		
+		String s1 = "hello";
+		String s2=  "ohell";
 	    
 	   System.out.println(isARotation(s1,s2));
 
 	}
 	public static boolean isARotation(String s1 , String s2){
 		
-		String s3 = s1+s2;
-		System.out.println(s3);
+		if(s1.length()!=s2.length()){
+			return false;
+		}
+		String s3 = s1+s1;
 		if(s3.contains(s2)){
 			return true;
 		}
+		
 		
 		
 		return false;
