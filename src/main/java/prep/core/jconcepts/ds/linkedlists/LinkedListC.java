2d3771172eddcb3a15955529d80b14fb45be7ef4
hareesha <hareesha1906@gmail.com>
2017-06-30 09:30:23 -0700

    kth node from last in a Singly linked list

--- src/main/java/prep/core/jconcepts/ds/linkedlists/LinkedListC.java
+++ src/main/java/prep/core/jconcepts/ds/linkedlists/LinkedListC.java
@@ -14,6 +14,11 @@ public class LinkedListC {
 	 */
 	
 	ListNode head = null;
+	
+	/**
+	 * Add to the end of the list
+	 * @param d
+	 */
 	void addToEnd(int d){
 		ListNode end = new ListNode(d);
 	
@@ -27,7 +32,11 @@ public class LinkedListC {
 		}
 		n.next = end;
 	}
-	
+	/**
+	 * Delete
+	 * @param d
+	 * @return
+	 */
 	ListNode delete(int d){
 		if(head==null){
 			return null;
@@ -47,6 +56,9 @@ public class LinkedListC {
 		return head;
 	}
 	
+	/**
+	 * Display elements of linked list
+	 */
 	void display(){
 		if(head==null){
 			return;
@@ -56,17 +68,51 @@ public class LinkedListC {
 			System.out.print("->"+n.data);
 			n = n.next;
 		}
+		System.out.println();
+	}
+	/**
+	 * remove kth node from last
+	 * @param k
+	 * @return
+	 */
+	Integer findkthNodeFromLast(int k){
+		
+		if(head==null){
+			return null;
+		}
+		
+		ListNode p1 = head;
+		ListNode p2 = head;
+		
+		for(int i=0;i<k;i++){
+			if(p1==null){
+				return null;
+			}
+			p1 = p1.next;
+			
+		}
+		
+		while(p1!=null){
+			p1=p1.next;
+			p2 = p2.next;
+		}
+		System.out.println(p2.data);
+		
+		return new Integer(p2.data);
 	}
 	public static void main(String[] args) {
 		LinkedListC ll= new LinkedListC();
 		ll.addToEnd(1);
 		ll.addToEnd(2);
 		ll.addToEnd(3);
-		ll.addToEnd(4);
+		ll.addToEnd(14);
+		ll.addToEnd(41);
+		ll.addToEnd(43);
 		ll.display();
 		ll.delete(3);
 		ll.delete(1);
 		ll.display();
+		ll.findkthNodeFromLast(3);
 	}
 
 }
